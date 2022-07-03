2698c1e842229ba83a37aae9651ee80cc72476cc
YaChugunov <informed.looser@gmail.com>
2022-07-03 15:12:46 +0300

    5 files modified, 2 added

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,10 @@ module.exports = {
     ],
   },
   devServer: {
-    port: 3000,
+    port: 3001,
     open: true,
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://localhost:3001',
     },
   },
   plugins: [
